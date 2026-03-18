f7659cd08b5c12d0dc8c09ea3fe1f0b05b66fb25
Juan Pablo Hernandez Corcuera <juanjphc@gmail.com>
2026-03-18 02:07:53 -0600

    fix: Names

--- src/ProtogenRhasky/RhaskyProtogenV3.h
+++ src/ProtogenRhasky/RhaskyProtogenV3.h
@@ -4,7 +4,7 @@
 #include "Camera/CameraManager/Implementations/HUB75DeltaCameras.h"
 #include "Controller/HUB75Controller.h"
 
-class CaraProtogenProject : public ProtogenProject {
+class RhaskyProtogenV3 : public ProtogenProject {
 private:
     HUB75DeltaCameraManager cameras;
     HUB75Controller controller = HUB75Controller(&cameras, 50, 50);
@@ -18,11 +18,11 @@ private:
 
     void LinkControlParameters() override {
         // Link morph weights to animation parameters
-        AddParameter(RhaskyProtoV3CustomExpression::AddEyebrow, faceModel.GetMorphWeightReference(cara protogen prueba::AddEyebrow), 15);
-        AddParameter(RhaskyProtoV3CustomExpression::MouthFrown, faceModel.GetMorphWeightReference(cara protogen prueba::MouthFrown), 15);
-        AddParameter(RhaskyProtoV3CustomExpression::EyeFrown, faceModel.GetMorphWeightReference(cara protogen prueba::EyeFrown), 15);
-        AddParameter(RhaskyProtoV3CustomExpression::HappySquint, faceModel.GetMorphWeightReference(cara protogen prueba::HappySquint), 15);
-        AddParameter(RhaskyProtoV3CustomExpression::AngryEyes, faceModel.GetMorphWeightReference(cara protogen prueba::AngryEyes), 15);
+        AddParameter(RhaskyProtoV3CustomExpression::AddEyebrow, faceModel.GetMorphWeightReference(RhaskyProtoV3CustomExpression::AddEyebrow), 15);
+        AddParameter(RhaskyProtoV3CustomExpression::MouthFrown, faceModel.GetMorphWeightReference(RhaskyProtoV3CustomExpression::MouthFrown), 15);
+        AddParameter(RhaskyProtoV3CustomExpression::EyeFrown, faceModel.GetMorphWeightReference(RhaskyProtoV3CustomExpression::EyeFrown), 15);
+        AddParameter(RhaskyProtoV3CustomExpression::HappySquint, faceModel.GetMorphWeightReference(RhaskyProtoV3CustomExpression::HappySquint), 15);
+        AddParameter(RhaskyProtoV3CustomExpression::AngryEyes, faceModel.GetMorphWeightReference(RhaskyProtoV3CustomExpression::AngryEyes), 15);
         
         // Add blink parameter
         AddBlinkParameter(faceModel.GetMorphWeightReference(RhaskyProtoV3CustomExpression::EyeFrown));
@@ -49,9 +49,26 @@ private:
         AddParameterFrame(RhaskyProtoV3CustomExpression::EyeFrown, 0.6f);
         AddMaterialFrame(Color::CBLUE);
     }
+    
+    void Surprised() {
+        AddParameterFrame(RhaskyProtoV3CustomExpression::AddEyebrow, 0.8f);
+        AddParameterFrame(RhaskyProtoV3CustomExpression::HappySquint, 0.5f);
+        AddMaterialFrame(Color::CRAINBOW);
+    }
+    
+    void Focused() {
+        AddParameterFrame(RhaskyProtoV3CustomExpression::EyeFrown, 0.7f);
+        AddParameterFrame(RhaskyProtoV3CustomExpression::AngryEyes, 0.3f);
+        AddMaterialFrame(Color::CWHITE);
+    }
+    
+    void Blink() {
+        AddParameterFrame(RhaskyProtoV3CustomExpression::EyeFrown, 1.0f);
+        AddMaterialFrame(Color::CBLACK);
+    }
 
 public:
-    CaraProtogenProject() : ProtogenProject(&cameras, &controller, 2, Vector2D(), Vector2D(192.0f, 94.0f), 22, 23, 8) {
+    RhaskyProtogenV3() : ProtogenProject(&cameras, &controller, 2, Vector2D(), Vector2D(192.0f, 94.0f), 22, 23, 8) {
         scene.AddObject(faceModel.GetObject());
         scene.AddObject(deltaDisplayBackground.GetObject());
 
@@ -93,5 +110,18 @@ public:
             case 6: Blink();    break;
             default: AudioReactiveGradientFace(); break;
         }
+    }
+
+    // Pure virtual function implementations required by ProtogenProject
+    void SpectrumAnalyzerCallback() override {
+        AddMaterialFrame(Color::CHORIZONTALRAINBOW, 0.8f);
+    }
+
+    void AudioReactiveGradientCallback() override {
+        AddMaterialFrame(Color::CHORIZONTALRAINBOW, 0.8f);
+    }
+
+    void OscilloscopeCallback() override {
+        AddMaterialFrame(Color::CHORIZONTALRAINBOW, 0.8f);
     }
 };
